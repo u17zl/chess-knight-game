ac9c7813723691c95f9c79ae64d5581a8ce0bb64
Kaiwen Luo <luokw8@gmail.com>
2019-12-06 13:18:29 +1100

    Updates

---
+++ precache-manifest.313df6cbaaec1744c14d3612f464af13.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a16c8f3aedbef923e407e1439929727f",
+    "url": "/chess-knight-game/index.html"
+  },
+  {
+    "revision": "85b717a648d6570dcf18",
+    "url": "/chess-knight-game/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "bf84c17dc7b1e2a7bec7",
+    "url": "/chess-knight-game/static/css/main.33870493.chunk.css"
+  },
+  {
+    "revision": "85b717a648d6570dcf18",
+    "url": "/chess-knight-game/static/js/2.98dd2b44.chunk.js"
+  },
+  {
+    "revision": "bf84c17dc7b1e2a7bec7",
+    "url": "/chess-knight-game/static/js/main.90283bf0.chunk.js"
+  },
+  {
+    "revision": "5ce8bcea691a0c560eaf",
+    "url": "/chess-knight-game/static/js/runtime-main.08057fe2.js"
+  },
+  {
+    "revision": "8dbd2875db66842ae2f859ef6f98b712",
+    "url": "/chess-knight-game/static/media/knight.8dbd2875.png"
+  },
+  {
+    "revision": "c60775b48d81cc34d9798ffa111b8141",
+    "url": "/chess-knight-game/static/media/target.c60775b4.png"
+  }
+]);
